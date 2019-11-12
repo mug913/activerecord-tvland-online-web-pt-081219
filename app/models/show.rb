7ec4acc99827e086a263631ff811d4838893ad98
Brad Alexander <brad.alexander913@gmail.com>
2019-11-12 03:10:30 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,14 @@
 class Show < ActiveRecord::Base
   has_many :actors
   has_many :characters
+  belongs_to :network
+
+  def actors_list
+    actors = []
+    self.actors do |a|
+      actors << a.full_name
+    end
+  end
+
 
 end
